ead9f679a392ff05121253ca1a09c3a9192af33c
Bryan Silva <brsilvau@gmail.com>
2018-09-28 21:55:15 -0300

    Initial commit

---
+++ app/src/main/java/scrumti/unabtiapp/MainActivity.java
@@ -0,0 +1,13 @@
+package scrumti.unabtiapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
